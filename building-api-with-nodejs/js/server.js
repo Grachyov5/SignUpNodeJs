e2dd90112860237309512d71ebedf40ae805fa41
Grachyov Igor <Grachyov_I@Aktobe.Railways.kz>
2020-03-02 10:11:02 +0600

    Устранена ошибка с оправкой данных на сервер

--- building-api-with-nodejs/js/server.js
+++ building-api-with-nodejs/js/server.js
@@ -11,40 +11,41 @@ app.use(express.static(__dirname + "/js/"));
 app.post('/signup', function(req, res) {  
   var options = {
     'method': 'POST',
-    'url': 'http://localhost:3001/users',
+    'url': 'http://localhost:9000/user/register',
     'headers': {
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify([{
-      "user":[{
-        "id":"null",
+    body: JSON.stringify({
+      "user":{
+        "id":null,
         "username":req.body.j_username,
         "password":req.body.j_password,
-        "passwordmd5":"null",
+        "passwordmd5":null,
         "ud_tb":0,
         "idea":0,
         "secretquestions":"секретный вопрос",
         "answer":"ответ",
         "enabled":0
-      }]},{
-        "userInfo":[{
-          "id":"null",
-          "username":req.body.j_username,
-          "name":req.body.nameUser,
-          "surname":req.body.surName,
-          "patronymic":req.body.patronymic,
-          "birthday":req.body.birthday,
-          "address":"null",
-          "branch":0,
-          "department":0,
-          "nti":0,
-          "line":0,
-          "region":0,
-          "danger":0,
-          "phone":req.body.phone,
-          "email":req.body.email,
-          "education":"null"
-        }]}])
+      },
+      "userInfo":{
+        "id":null,
+        "username":req.body.j_username,
+        "name":req.body.nameUser,
+        "surname":req.body.surName,
+        "patronymic":req.body.patronymic,
+        "birthday":req.body.birthday,
+        "address":null,
+        "branch":0,
+        "department":0,
+        "nti":0,
+        "line":0,
+        "region":0,
+        "danger":0,
+        "phone":req.body.phone,
+        "email":req.body.email,
+        "education":null
+      }
+    })
   };
   request(options, function (error, response) { 
     // if (error) throw new Error(error);
